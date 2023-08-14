4034bdd198ba9ab4bdb7a2b0121d62aa8546cc2a
Andre Vargas Roo <a.vargas252@icloud.com>
2023-08-14 12:19:32 -0400

    added home endpoint

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from flask_restful import Resource
 from config import api, app
 from models import db, Bird
 
+class Home(Resource):
+    def get(self):
+        return 'Yes this works, go to /birds ;)'
+
 class Birds(Resource):
 
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
 
+api.add_resource(Home, '/')
 api.add_resource(Birds, '/birds')
 
 if __name__ == '__main__':
