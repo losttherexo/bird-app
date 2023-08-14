be767e8d956075fcf7129ad2af02c6cf43f526cd
Andre Vargas Roo <a.vargas252@icloud.com>
2023-08-14 12:35:50 -0400

    add BirdsByID

--- app.py
+++ app.py
@@ -13,9 +13,15 @@ class Birds(Resource):
     def get(self):
         birds = [bird.to_dict() for bird in Bird.query.all()]
         return make_response(jsonify(birds), 200)
+    
+class BirdByID(Resource):
+    def get(self, id):
+        bird = Bird.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(bird), 200)
 
 api.add_resource(Home, '/')
 api.add_resource(Birds, '/birds')
+api.add_resource(BirdByID, '/birds/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
